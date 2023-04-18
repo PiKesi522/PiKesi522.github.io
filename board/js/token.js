f55c7b5b6c4f563154b13f3e2d10a14277e7746a
PiKesi <10192100571@stu.ecnu.edu.cn>
2023-04-18 23:19:47 +0800

    modify

--- board/js/token.js
+++ board/js/token.js
@@ -1,2 +1,2 @@
-const token = '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';
+const token = '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';
 const repo = 'PiKesi522/PiKesi522.github.io'; // 替换为您的仓库所有者和名称
